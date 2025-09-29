d525e179ad931ddc4706f0d7c8946e45388b6fc6
suhaibkhan <johnxuhaib@gmail.com>
2025-09-29 22:47:07 +0530

    5.1

--- 5.1/controllers/product.controller.js
+++ 5.1/controllers/product.controller.js
@@ -1,24 +1,72 @@
-const products = require('../models/products.models')
-const createProduct = async(req, res) => {
-    try {
-        const product = new products(req.body)
-       await product.save()
-        res.status(201).send(product)
-    }
-    catch (err) {
-        res.status(400).send(err.message)
-    }
-}
+const products = require('../models/products.models');
 
+// Create product
+const createProduct = async (req, res) => {
+    try {
+        const product = new products(req.body);
+        await product.save();
+        res.status(201).send(product);
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+};
+
+// Get all products
 const getProducts = async (req, res) => {
     try {
         const allProducts = await products.find();
-        res.status(200).json(allProducts)
+        res.status(200).json(allProducts);
+    } catch (err) {
+        res.status(400).send(err.message);
     }
-    catch (err) {
-        res.status(400).send(err.message)
-    }
-    
-}
+};
 
-module.exports = { createProduct, getProducts }
+// Get one product by ID
+const getProductById = async (req, res) => {
+    try {
+        const product = await products.findById(req.params.id);
+        if (!product) {
+            return res.status(404).send("Product not found");
+        }
+        res.status(200).json(product);
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+};
+
+// Update product by ID
+const updateProduct = async (req, res) => {
+    try {
+        const product = await products.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+        );
+        if (!product) {
+            return res.status(404).send("Product not found");
+        }
+        res.status(200).json(product);
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+};
+
+// Delete product by ID
+const deleteProduct = async (req, res) => {
+    try {
+        const product = await products.findByIdAndDelete(req.params.id);
+        if (!product) {
+            return res.status(404).send("Product not found");
+        }
+        res.status(200).send("Product deleted successfully");
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+};
+
+module.exports = { 
+    createProduct, 
+    getProducts, 
+    getProductById, 
+    updateProduct, 
+    deleteProduct 
+};
--- 5.1/index.js
+++ 5.1/index.js
@@ -1,19 +1,32 @@
-const express = require('express')
-const connectDB = require('./db/db')
-const { createProduct,getProducts} =require('./controllers/product.controller')
-const app = express()
-const port = 3000
+const express = require('express');
+const connectDB = require('./db/db');
+const { 
+  createProduct, 
+  getProducts, 
+  getProductById, 
+  updateProduct, 
+  deleteProduct 
+} = require('./controllers/product.controller');
+
+const app = express();
+const port = 3000;
+
 app.use(express.json());
 
+// Root route
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.post('/products',createProduct)
-app.get('/products',getProducts)
+// Product routes
+app.post('/products', createProduct);      // Create
+app.get('/products', getProducts);         // Get all
+app.get('/products/:id', getProductById);  // Get one by ID
+app.put('/products/:id', updateProduct);   // Update by ID
+app.delete('/products/:id', deleteProduct);// Delete by ID
 
-
+// Start server
 app.listen(port, () => {
   connectDB();
-  console.log(`server running at http://localhost:${port}`)
-})
+  console.log(`Server running at http://localhost:${port}`);
+});
